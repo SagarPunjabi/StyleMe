de27e40dd338dab7762e3f6ebae9e591d46d180f
Sagar Punjabi <=sagar.punjabi@yahoo.com>
2021-10-14 18:20:41 -0400

    added signup route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :clothes
   resources :users
   root 'application#welcome'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
