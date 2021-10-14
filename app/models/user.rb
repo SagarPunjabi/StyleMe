d3a693c1f0b2b3375f08f20f8c2e0ec5a722e5f4
Sagar Punjabi <=sagar.punjabi@yahoo.com>
2021-10-13 23:14:39 -0400

    added secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     before_save { email.downcase! }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 7 }
 end
