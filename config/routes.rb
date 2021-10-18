6f291f0a7a471e136569470e56abb4d8ff33061d
Khadija T <52125592+t03t@users.noreply.github.com>
2021-10-17 21:01:57 -0400

    login-homepage conversion

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#login'
   get 'sessions/new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :clothes
   resources :users
-  root 'application#welcome'
+  # root 'application#welcome'
   get '/signup', to: 'users#new'
   get '/uploadclothes', to: 'clothes#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
