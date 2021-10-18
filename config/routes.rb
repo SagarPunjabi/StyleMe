a1c32f54cba049946c9e10b8dbf6ad664c217d3b
Khadija T <52125592+t03t@users.noreply.github.com>
2021-10-17 21:12:52 -0400

    still fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  # get 'sessions/new'
+  get 'sessions/new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
