4dfbed9d194e74988c4585dc74bc40baa467226b
RyanPyatetsky <64447359+RyanPyatetsky@users.noreply.github.com>
2021-11-26 18:20:28 -0500

    fixing heroku error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,5 @@
 class SessionsController < ApplicationController
-
-  
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
@@ -18,9 +15,8 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out
-    redirect_to root
+    redirect_to root_url
   end
 
-  def show
-  end
+  def show; end
 end
